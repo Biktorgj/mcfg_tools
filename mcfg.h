0e42f8a0c90383d28615d2a2416e3a4037511ba9
Biktor <biktor@biktorgj.com>
2022-08-15 08:37:19 +0200

    It seems we mostly loop correctly

--- mcfg.h
+++ mcfg.h
@@ -12,7 +12,7 @@
 
 #define MCFG_FILE_HEADER_MAGIC "MCFG"
 #define ELF_OFFSET 8192
-#define VERSION_DATA 4995
+#define VERSION_NUM 4995
 
 struct mcfg_file_header {
     unsigned char magic[4]; //MCFG
@@ -22,30 +22,30 @@ struct mcfg_file_header {
     uint8_t padding[4]; // 0x00
 } __attribute__((packed));
 
- struct mcfg_carrier_specific_version { // unsure
-    uint8_t const1; // 0x83
-    uint8_t const2; // 0x13;
+ struct mcfg_sub_version_data { // unsure
+    uint16_t version; // 0x83 0x13
     uint16_t unknown1; // 0x04 0x00
     uint16_t unknown2; // 0x01 0x00 || 0x03 0x00 
     uint8_t unknown3; // 0xe0
     uint8_t unknown4; // 0x02
 } __attribute__((packed));
 
+struct mcfg_config_item {
+    uint16_t id; // EFS NV element?
+    uint16_t payload_size; // Size of the rest of the item
+    uint8_t *payload[0];
+} __attribute__((packed));
+
 struct mcfg_item {
     uint32_t item_id; // EFS NV element?
-    uint8_t unk1; // 0x01
-    uint8_t unkn2; // 0x09
+    uint8_t u1; // 0x01
+    uint8_t u2; // 0x09 0x29?
     uint16_t padding; // 0x00 0x00
-    uint16_t unkn3; // 0x47 0x00, 0xb2 0x00...
-    uint16_t payload_size; // Size of the rest of the item
-    uint8_t *payload[0];
+    struct mcfg_config_item item;
 } __attribute__((packed));
 
-struct mcfg_config_item {
-    uint16_t item_id; // EFS NV element?
-    uint16_t payload_size; // Size of the rest of the item
-    uint8_t *payload[0];
-} __attribute__((packed));
-
+enum {
+    MCFG_CARRIER_NAME = 0x00000019,
+};
 
 #endif
--- read_mcfg.c
+++ read_mcfg.c
@@ -9,7 +9,19 @@
 
 uint8_t *file_buff;
 size_t sz;
+/* So, some notes here: 
+1. 16 byte header
+2. A second header, seemingly 8 byte in size
+Then we have some sort of encapsulated QMI like message, with
+[uint32_t],[u8],[u8],0x00,0x00,[u16] ID,[u16] Length and [u8*]data
+There are some magic numbers for whatever the type of the data is
+So, first things we get the header to know the number of items
 
+
+
+
+
+*/
 void showHelp() {
   fprintf(stdout, "Usage:\n");
   fprintf(stdout, "  read_mcfg -i INPUT_FILE \n");
@@ -20,7 +32,7 @@ void showHelp() {
 int check_file_header() {
   struct mcfg_file_header *mcfg_head;
   mcfg_head = (struct mcfg_file_header *)(file_buff + ELF_OFFSET);
-  struct mcfg_config_item *tptr;
+  struct mcfg_item *tptr;
   if (memcmp(mcfg_head->magic, MCFG_FILE_HEADER_MAGIC, 4) == 0) {
     fprintf(stdout,
             "Header is OK:\n"
@@ -32,16 +44,38 @@ int check_file_header() {
             mcfg_head->config_type < 1 ? "HW" : "SW", mcfg_head->no_of_items);
     
     // First element is some kind of subheader with the carrier name?
-    tptr = (struct mcfg_config_item*) (file_buff + ELF_OFFSET + sizeof(struct mcfg_file_header));
-
+    struct mcfg_sub_version_data *version = (struct mcfg_sub_version_data *)(file_buff + ELF_OFFSET + sizeof(struct mcfg_file_header));
+    if (version->version == VERSION_NUM) {
+        fprintf(stdout, "Version is %.4x |%.4x |%.2x |%.2x\n", version->unknown1, version->unknown2, version->unknown3, version->unknown4);
+    } else {
+        fprintf(stderr, "Oopsies, something is wrong\n");
+        return -EINVAL;
+    }
+    version = NULL;
+    
+    
+    // We assign the first element by hand, then we will go moving the offset as we parse
+    tptr = (struct mcfg_item*) (file_buff + ELF_OFFSET + sizeof(struct mcfg_file_header)+ sizeof(struct mcfg_sub_version_data));
+    int newoffset = 0;
     for (int i = 0; i < mcfg_head->no_of_items; i++) {
         fprintf(stdout, "Trying to read item %i...\n", i);
-        switch (tptr->item_id) {
-            case VERSION_DATA:
-                fprintf(stdout, "Version data, size %i\n", tptr->payload_size);
-                break;
-        }
+        fprintf(stdout, "Item ID: %.4x | u1: %.2x | u2 %.2x\n", tptr->item_id, tptr->u1, tptr->u2);
+        fprintf(stdout, "Item inside: %.4x of size %.4x\n", tptr->item.id, tptr->item.payload_size);
+        uint8_t buffer[256];
+        if (tptr->item.payload_size < 256)
+            memcpy(buffer, tptr->item.payload, tptr->item.payload_size);
+        fprintf(stdout, "Payload: %s\n", buffer);
+
+
+        newoffset+= (sizeof(struct mcfg_item)+ htole16(tptr->item.payload_size ));
+        tptr = (struct mcfg_item*) (file_buff + ELF_OFFSET + sizeof(struct mcfg_file_header)+ sizeof(struct mcfg_sub_version_data) +newoffset);
     }
+    // End of the file
+    fprintf(stdout, "end of the file:\n");
+    for (int k = (ELF_OFFSET + sizeof(struct mcfg_file_header)+ sizeof(struct mcfg_sub_version_data) +newoffset); k < sz; k++) {
+        fprintf(stdout, "%.2x ", file_buff[k]);
+    }
+    fprintf(stdout, "\n");
   } else {
     fprintf(stderr, "ERROR: Header doesn't match\n");
     return -EINVAL;
