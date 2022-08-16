017a5c431fe7a098ce7217a3ef753eb48806900e
Biktor <biktor@biktorgj.com>
2022-08-16 07:49:37 +0200

    Some progress, files seem to be splitted in parts, signaled by 01 and 02 with sizes defined inside the block. So that's why I have a buffer overflow ;)

--- mcfg.h
+++ mcfg.h
@@ -1,3 +1,4 @@
+#include <argp.h>
 #include <endian.h>
 #include <fcntl.h>
 #include <stdint.h>
@@ -5,7 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <argp.h>
 
 #ifndef __MCFG_H__
 #define __MCFG_H__
@@ -15,44 +15,59 @@
 #define VERSION_NUM 4995
 
 struct mcfg_file_header {
-    unsigned char magic[4]; //MCFG
-    uint16_t format_version; // 0x02 0x00 
-    uint16_t config_type; // MCFG_HW is 0, MCFG_SW is 1
-    uint32_t no_of_items; // Number of items in the file 
-    uint8_t padding[4]; // 0x00
+  unsigned char magic[4];  // MCFG
+  uint16_t format_version; // 0x02 0x00
+  uint16_t config_type;    // MCFG_HW is 0, MCFG_SW is 1
+  uint32_t no_of_items;    // Number of items in the file
+  uint8_t padding[4];      // 0x00
 } __attribute__((packed));
 
- struct mcfg_sub_version_data { // unsure
-    uint16_t version; // 0x83 0x13
-    uint16_t unknown1; // 0x04 0x00
-    uint16_t unknown2; // 0x01 0x00 || 0x03 0x00 
-    uint8_t unknown3; // 0xe0
-    uint8_t unknown4; // 0x02
+struct mcfg_sub_version_data { // unsure
+  uint16_t version;            // 0x83 0x13
+  uint16_t unknown1;           // 0x04 0x00
+  uint16_t unknown2;           // 0x01 0x00 || 0x03 0x00
+  uint8_t unknown3;            // 0xe0
+  uint8_t unknown4;            // 0x02
 } __attribute__((packed));
 
 struct mcfg_config_item {
-    uint16_t id; // EFS NV element?
-    uint16_t payload_size; // Size of the rest of the item
-    uint8_t *payload[0];
+  uint16_t id;           // EFS NV element?
+  uint16_t payload_size; // Size of the rest of the item
+  uint8_t *payload[0];
 } __attribute__((packed));
 
 struct mcfg_item {
-    uint32_t item_id; // EFS NV element?
-    uint8_t u1; // 0x01
-    uint8_t u2; // 0x09 0x29?
-    uint16_t padding; // 0x00 0x00
-    struct mcfg_config_item item;
+  uint32_t item_id; // EFS NV element?
+  uint8_t u1;       // 0x01 || <-- ITEM TYPE?
+  uint8_t u2;       // 0x09 0x29? <-- Attributes?
+  uint16_t padding; // 0x00 0x00
+  struct mcfg_config_item item;
 } __attribute__((packed));
 
 // Base item IDs
 enum {
-    MCFG_CARRIER_NAME = 0x00000019,
+  MCFG_CARRIER_NAME = 0x00000019,
 };
 
 // Config Item IDs
 enum {
-    MCFG_EFS_FILE = 0x0002,
-    MCFG_FOOTER = 0x00A1,
+  MCFG_EFS_FILE = 0x02,
+  MCFG_FOOTER = 0xA1,
+};
+
+/* Item types, borrowed from
+ * https://github.com/JohnBel/EfsTools/blob/master/EfsTools/Mbn/ItemType.cs */
+enum {
+  MCFG_ITEM_TYPE_NV = 0x01,
+  MCFG_ITEM_TYPE_NVFILE = 0x02,
+  MVFG_ITEM_TYPE_FILE = 0x04,
+};
+
+/* Attributes */
+enum {
+    ATTRIB_MODE_09 = 0x09,
+    ATTRIB_MODE_29 = 0x29,
+    ATTRIB_MODE_2A = 0x2A,
 };
 
 #endif
--- read_mcfg.c
+++ read_mcfg.c
@@ -9,6 +9,10 @@
 
 uint8_t *file_buff;
 size_t sz;
+size_t current_file_offset;
+size_t prev_file_offset;
+struct mcfg_file_header mcfg_file;
+
 /* So, some notes here: 
 1. 16 byte header
 2. A second header, seemingly 8 byte in size
@@ -32,6 +36,54 @@ void showHelp() {
 int check_file_header() {
   struct mcfg_file_header *mcfg_head;
   mcfg_head = (struct mcfg_file_header *)(file_buff + ELF_OFFSET);
+  if (memcmp(mcfg_head->magic, MCFG_FILE_HEADER_MAGIC, 4) == 0) {
+    fprintf(stdout,
+            "Header is OK:\n"
+            "\t- Magic: %s \n"
+            "\t- Format: %i \n"
+            "\t- Config type: %s\n"
+            "\t- Number of elements in file: %i\n",
+            mcfg_head->magic, mcfg_head->format_version,
+            mcfg_head->config_type < 1 ? "HW" : "SW", mcfg_head->no_of_items);
+    mcfg_file.no_of_items = mcfg_head->no_of_items;
+    mcfg_file.config_type = mcfg_head->config_type;
+    mcfg_file.format_version = mcfg_head->format_version;
+
+    // First element is some kind of subheader with the carrier name?
+    // Offset gets poluted with nv data
+    struct mcfg_sub_version_data *version = (struct mcfg_sub_version_data *)(file_buff + ELF_OFFSET + sizeof(struct mcfg_file_header));
+    if (version->version == VERSION_NUM) {
+        fprintf(stdout, "Version is %.4x |%.4x |%.2x |%.2x\n", version->unknown1, version->unknown2, version->unknown3, version->unknown4);
+    } else {
+        fprintf(stderr, "Oopsies, something is wrong\n");
+        return -EINVAL;
+    }
+    version = NULL;
+    
+    fprintf(stdout, "--------------------------\n");
+  } else {
+    fprintf(stderr, "ERROR: Header doesn't match\n");
+    return -EINVAL;
+  }
+  return 0;
+}
+/*
+Example EFS/NV item:
+                                               ........... We start here, u32 ID
+00002180  02 00 08 00 05 00 00 00  00 01 00 00 29 00 00 00  |............)...|
+          u1 u2 [pad] fname u16sz  <----------------------
+00002190  02 09 00 00 01 00 12 00  2f 73 64 2f 72 61 74 5f  |......../sd/rat_|
+          -----------------------------> fcont u16sz <----
+000021a0  61 63 71 5f 6f 72 64 65  72 00 02 00 07 00 03 e7  |acq_order.......|
+          -------------> || NEXT FILE...
+000021b0  00 03 09 05 03 39 00 00  00 02 09 00 00 01 00 27  |.....9.........'|
+*/
+
+int dump_contents() {
+  fprintf(stdout, "[%s:%i]: Start \n", __func__, __LINE__);
+  return 0;
+  struct mcfg_file_header *mcfg_head;
+  mcfg_head = (struct mcfg_file_header *)(file_buff + ELF_OFFSET);
   struct mcfg_item *tptr;
   if (memcmp(mcfg_head->magic, MCFG_FILE_HEADER_MAGIC, 4) == 0) {
     fprintf(stdout,
@@ -42,8 +94,12 @@ int check_file_header() {
             "\t- Number of elements in file: %i\n",
             mcfg_head->magic, mcfg_head->format_version,
             mcfg_head->config_type < 1 ? "HW" : "SW", mcfg_head->no_of_items);
+    mcfg_file.no_of_items = mcfg_head->no_of_items;
+    mcfg_file.config_type = mcfg_head->config_type;
+    mcfg_file.format_version = mcfg_head->format_version;
     
     // First element is some kind of subheader with the carrier name?
+    // Offset gets poluted with nv data
     struct mcfg_sub_version_data *version = (struct mcfg_sub_version_data *)(file_buff + ELF_OFFSET + sizeof(struct mcfg_file_header));
     if (version->version == VERSION_NUM) {
         fprintf(stdout, "Version is %.4x |%.4x |%.2x |%.2x\n", version->unknown1, version->unknown2, version->unknown3, version->unknown4);
@@ -56,33 +112,47 @@ int check_file_header() {
     fprintf(stdout, "--------------------------\n");
     // We assign the first element by hand, then we will go moving the offset as we parse
     tptr = (struct mcfg_item*) (file_buff + ELF_OFFSET + sizeof(struct mcfg_file_header)+ sizeof(struct mcfg_sub_version_data));
+    current_file_offset = ELF_OFFSET + sizeof(struct mcfg_file_header)+ sizeof(struct mcfg_sub_version_data);
+
     size_t newoffset = 0;
     
     // I'm not counting as I should
     for (int i = 0; i < mcfg_head->no_of_items; i++) {
-        fprintf(stdout, "Trying to read item %i...\n", i);
-        fprintf(stdout, " * Item ID: %.4x | u1: %.2x | u2 %.2x\n", tptr->item_id, tptr->u1, tptr->u2);
-        fprintf(stdout, " * Item inside: %.4x of size %.4x\n", tptr->item.id, tptr->item.payload_size);
-        uint8_t buffer[256];
-        memset(buffer, 0, 256);
-        if (tptr->item.payload_size < 256)
+        fprintf(stdout, "Item #%i\n", i);
+        fprintf(stdout, " |- Block ID: %.8x \n |- u1: %.2x\n |- u2: %.2x\n", tptr->item_id, tptr->u1, tptr->u2);
+        fprintf(stdout, " |-- Item: %.4x of size %.4x\n", tptr->item.id, tptr->item.payload_size);
+        uint8_t buffer[4096];
+        memset(buffer, 0, 4096);
+        if (tptr->item.payload_size < 4096)
             memcpy(buffer, tptr->item.payload, tptr->item.payload_size);
-        fprintf(stdout, " * Payload: %s\n", buffer);
+        fprintf(stdout, " |-- Payload: %s\n", buffer);
+        if (tptr->u1 == 0x02 && tptr->u2 == 0x09) {
+            fprintf(stdout, " |-- NV Item: %s\n", buffer);
 
-        switch(le16toh(tptr->item.id)) {
-            case MCFG_EFS_FILE:
-            fprintf(stdout, "NV Item: %s\n", buffer);
+        }
+        switch(tptr->u1) {
+            case MCFG_ITEM_TYPE_NV:
+            fprintf(stdout, "MCFG_ITEM_TYPE_NV: %s\n", buffer);
+
             break;
-            case MCFG_FOOTER:
-            fprintf(stdout, "Footer\n");
+            case MCFG_ITEM_TYPE_NVFILE:
+            fprintf(stdout, "MCFG_ITEM_TYPE_NVFILE: %s\n", buffer);
+            
+            break;
+            case MVFG_ITEM_TYPE_FILE:
+            fprintf(stdout, "MVFG_ITEM_TYPE_FILE: %s\n", buffer);
             break;
             default:
             fprintf(stdout, "Unkonwn ID %.4x\n", tptr->item.id);
             break;
         }
-        newoffset+= (sizeof(struct mcfg_item)+ htole16(tptr->item.payload_size ));
-        fprintf(stdout, " * Next offset: %ld\n", newoffset);
 
+        newoffset+= (sizeof(struct mcfg_item)+ htole16(tptr->item.payload_size ));
+        fprintf(stdout, " * Next offset: %ld\n", ELF_OFFSET + sizeof(struct mcfg_file_header)+ sizeof(struct mcfg_sub_version_data) +newoffset);
+        if (ELF_OFFSET + sizeof(struct mcfg_file_header)+ sizeof(struct mcfg_sub_version_data) +newoffset > sz) {
+          fprintf(stdout, "Err: overflowed!\n");
+          return 1;
+        }
         tptr = (struct mcfg_item*) (file_buff + ELF_OFFSET + sizeof(struct mcfg_file_header)+ sizeof(struct mcfg_sub_version_data) +newoffset);
     }
     // End of the file
@@ -134,12 +204,18 @@ int main(int argc, char *argv[]) {
   file_buff = malloc(sz);
   fseek(fp, 0L, SEEK_SET);
   fread(file_buff, sz, 1, fp);
+
+  current_file_offset = prev_file_offset = 0;
   fclose(fp);
   if (check_file_header() < 0) {
     free(file_buff);
     return 1;
   }
 
+  if (dump_contents() < 0) {
+    free(file_buff);
+    return 1;
+  }
   free(file_buff);
   return 0;
 }
